fbe207ede7b02b9f56c388a6669855d6eb2094bb
Mathew Attlee <hello@codeinabox.com>
2015-05-31 22:08:45 +0100

    Imported application base from github.com/pebble/pebblejs

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+
+var main = new UI.Card({
+  title: 'Pebble.js',
+  subtitle: 'Hello World!',
+  body: 'Press any button.'
+});
+
+main.show();
